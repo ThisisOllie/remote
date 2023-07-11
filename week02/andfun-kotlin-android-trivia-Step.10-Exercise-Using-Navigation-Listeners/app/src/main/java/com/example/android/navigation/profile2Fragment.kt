cda7ccaaa9ec30f49d7b004c38f8b04f6742af28
ThisisOllie <aus.celestial@gmail.com>
2023-07-11 14:02:59 +0800

    2nd update

--- week02/andfun-kotlin-android-trivia-Step.10-Exercise-Using-Navigation-Listeners/app/src/main/java/com/example/android/navigation/profile2Fragment.kt
+++ week02/andfun-kotlin-android-trivia-Step.10-Exercise-Using-Navigation-Listeners/app/src/main/java/com/example/android/navigation/profile2Fragment.kt
@@ -16,7 +16,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [profile2Fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class profile2Fragment : Fragment() {
+class Profile2Fragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -49,7 +49,7 @@ class profile2Fragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            profile2Fragment().apply {
+            Profile2Fragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
